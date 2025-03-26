35a57adc2615a445c6bea1486f44b5a7ec0de318
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2025-03-26 22:48:01 +0800

    [frontend] Show alerts in meter page. Fixes #236 (#249)

--- frontend/svelte-app/src/routes/(private)/meter/[meter_id]/+page.server.ts
+++ frontend/svelte-app/src/routes/(private)/meter/[meter_id]/+page.server.ts
@@ -1,7 +1,7 @@
 import type { PageServerLoad } from './$types';
 
 import { AuthorizationCookie } from '@utils/Constants';
-import { getMeasurements, getFluidMeter } from '@api/FluidMeter';
+import { getMeasurements, getFluidMeterAlerts } from '@api/FluidMeter';
 
 export const load: PageServerLoad = async ({ params, cookies }) => {
   const token = cookies.get(AuthorizationCookie);
@@ -11,13 +11,13 @@ export const load: PageServerLoad = async ({ params, cookies }) => {
       error: 'No authorization token'
     };
   }
-  const [series, meter_data] = await Promise.all([
+  const [series, alerts] = await Promise.all([
     await getMeasurements(token, params.meter_id),
-    await getFluidMeter(token, params.meter_id)
+    await getFluidMeterAlerts(token, params.meter_id)
   ]);
 
   return {
-    meter_data,
+    alerts,
     series
   };
 };
