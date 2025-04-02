feec24ab2ddf9280513893f91287bd4ae0356a1a
Adrian Ancona Novelo <soonick5@yahoo.com.mx>
2025-04-02 17:15:05 +0800

    [frontend] Delete frontend. Moved to console repo (#261)

--- frontend/svelte-app/src/routes/(public)/email-verification/[token]/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { emailVerification } from '@api/User';
-
-export const load: PageServerLoad = async ({ params }) => {
-  return {
-    status: await emailVerification(params.token)
-  };
-};
